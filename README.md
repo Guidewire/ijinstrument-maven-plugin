f190ace8d3b65456c8a03c7e043aa0b9f07459b2
Ivan Dubrov <idubrov@guidewire.com>
2014-02-03 09:14:09 -0800

    Cleaning up, applying Apache license

--- README.md
+++ README.md
@@ -5,9 +5,3 @@ The following mojos are provided by this plugin:
 
 * instrument-forms instrument forms classes
 * instrument-notnull instrument @NotNull annotations
-
-Building and releasing
-----------------------
-
-This project is created according to the [Maven Best Practices](http://wiki/index.php/Maven/Best_Practices).
-
--- src/main/java/com/guidewire/build/plugins/ijinstrument/InstrumentForms.java
+++ src/main/java/com/guidewire/build/plugins/ijinstrument/InstrumentForms.java
@@ -1,10 +1,24 @@
+/**
+ * Copyright 2013-2014 Guidewire Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.guidewire.build.plugins.ijinstrument;
 
 import com.google.common.collect.Lists;
 import com.intellij.ant.InstrumentationUtil;
 import com.intellij.ant.PrefixedPath;
 import com.intellij.ant.PseudoClassLoader;
-import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -17,8 +31,6 @@ import org.apache.tools.ant.Project;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
 /**
